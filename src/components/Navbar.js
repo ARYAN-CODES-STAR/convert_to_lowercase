6e30fa2782eb435fd92f0ee5f9b8a78d80e658ab
Aryan Sharma <78686630+ARYAN-CODES-STAR@users.noreply.github.com>
2022-12-15 21:27:42 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Navbar() {
+  return (
+    <>
+     
+    </> 
+  )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string
+}
